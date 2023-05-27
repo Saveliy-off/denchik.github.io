f633d81dfc56dfa51dabfef5b4f4cda3ff0d071c
Saveliy <81488074+Saveliy-off@users.noreply.github.com>
2023-05-27 14:12:54 +0300

    1.0

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     console.log('start code..')
 
     $('button').on('click', function() {
+        console.log('heelo')
             $('.centerdiv').toggleClass('none');
     })
 
